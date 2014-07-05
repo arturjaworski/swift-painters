b54222b134d0be0b609400e689c5e88a882037dd
Artur Jaworski <kontakt@arturjaworski.pl>
2014-07-05 13:54:59 +0200

    initial commit

---
+++ Swift Crunch/Swift Crunch/GameScene.swift
@@ -0,0 +1,53 @@
+//
+//  GameScene.swift
+//  Swift Crunch
+//
+//  Created by Artur Jaworski on 05.07.2014.
+//  Copyright (c) 2014 brckt. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    var desiredPoint: CGPoint = CGPoint(x: 320, y: 0)
+    var currentPoint: CGPoint = CGPoint(x: 160, y: 160)
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Hello, World!";
+        myLabel.fontSize = 65;
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        
+        self.addChild(myLabel)
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            sprite.xScale = 0.2
+            sprite.yScale = 0.2
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
+
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        
+        
+    }
+}
