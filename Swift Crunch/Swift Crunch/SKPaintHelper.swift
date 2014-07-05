3753d1c727df1450b76f859f265fc662ac11218b
Maciej Mucha <mmucha@MacBook-Air-Kamila.local>
2014-07-05 19:15:10 +0200

    Painting in the center

--- Swift Crunch/Swift Crunch/SKPaintHelper.swift
+++ Swift Crunch/Swift Crunch/SKPaintHelper.swift
@@ -20,19 +20,8 @@ class SKPaintHelper {
     var imageSize: CGSize = CGSizeMake(320, 320)
     var image: UIImage
     
-    //var paintArray: Array<Array<UIColor>>
     init() {
         self.image = UIImage.imageWithColor(UIColor.clearColor(), size: self.imageSize)
-        
-        /*self.paintArray = Array()
-        for i in 0..(imageSize.width) {
-            var iArray: Array<UIColor> = Array()
-            for j in 0..(imageSize.height) {
-                var color: UIColor = UIColor(white: 0.0, alpha: 0.0)
-                iArray.insert(color, atIndex: Int(j))
-            }
-            self.paintArray.insert(iArray, atIndex: Int(i))
-        }*/
     }
     
     func texture() -> SKTexture {
@@ -41,7 +30,6 @@ class SKPaintHelper {
     
     func paintCircle(point: CGPoint, color: UIColor, width: CGFloat) {
         UIGraphicsBeginImageContext(self.image.size);
-        //self.image.drawAtPoint(point)
         var context: CGContextRef = UIGraphicsGetCurrentContext();
         
         CGContextScaleCTM(context, 1, -1);
@@ -50,7 +38,7 @@ class SKPaintHelper {
         CGContextDrawImage(context, CGRectMake(0, 0, self.image.size.width, self.image.size.height), self.image.CGImage);
         
         CGContextSetFillColorWithColor(context, color.CGColor)
-        CGContextFillEllipseInRect(context, CGRectMake(point.x, point.y, width, width))
+        CGContextFillEllipseInRect(context, CGRectMake(point.x - width/2, point.y - width/2, width, width))
         
         var newImage : UIImage = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
@@ -61,35 +49,4 @@ class SKPaintHelper {
     func paintLine(fromPoint: CGPoint, toPoint: CGPoint, color: UIColor, width: CGFloat) {
         
     }
-    
-    /*func createImage() -> UIImage {
-        
-        
-        var imageRect: CGRect = CGRectMake(0, 0, imageSize.width, imageSize.height)
-        var sampleImage: UIImageView = UIImageView(frame: imageRect)
-        
-        UIGraphicsBeginImageContext(imageRect.size)
-        var context: CGContextRef = UIGraphicsGetCurrentContext();
-        
-        CGContextSaveGState(context);
-        CGContextDrawImage(context, imageRect, sampleImage.image.CGImage);
-        
-        for i in 0..(imageRect.width) {
-            for j in 0..(imageRect.height) {
-                var color: UIColor = paintArray[Int(i)][Int(j)]
-                var red: CGFloat = 0.0
-                var green: CGFloat = 0.0
-                var blue: CGFloat = 0.0
-                var alpha: CGFloat = 0.0
-                
-                color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-                CGContextSetRGBFillColor(context, red, green, blue, alpha)
-                CGContextFillRect(context, CGRectMake(i, j, 1, 1))
-            }
-        }
-
-        CGContextRestoreGState(context);
-        var img: UIImage = UIGraphicsGetImageFromCurrentImageContext();
-        return img
-    }*/
 }
