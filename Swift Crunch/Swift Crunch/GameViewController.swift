990009dfd2424de7a2bc09886b0b06f4f07da7b4
Artur Jaworski <kontakt@arturjaworski.pl>
2014-07-06 14:09:00 +0200

    changed time

--- Swift Crunch/Swift Crunch/GameViewController.swift
+++ Swift Crunch/Swift Crunch/GameViewController.swift
@@ -37,7 +37,7 @@ class GameViewController: UIViewController {
     var timer: NSTimer?
     var roundCounter: Int = 0
     
-    var roundTime: NSTimeInterval = 7.0
+    var roundTime: NSTimeInterval = 4.0
     var maxRounds: Int = 10
     
     override func viewDidLoad() {
